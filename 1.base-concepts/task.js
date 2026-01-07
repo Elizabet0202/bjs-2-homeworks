1ed8ffcd0ec839cfc26edd310d6ddfff303e25e2
Elizaveta <yelizaveta.baklanova@gmail.com>
2026-01-07 20:14:23 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  const discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant < 0) {
+    return arr;
+}
+else if (discriminant === 0) {
+  arr.push(-b / (2 * a));
+}
+else if (discriminant > 0) {
+  arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+  arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+}
+return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+   const monthPercent = percent / 100 / 12;
+  const creditBody = amount - contribution;
+  const monthPayment = creditBody * (monthPercent + (monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1)));
+  return +(monthPayment * countMonths).toFixed(2);
 }
