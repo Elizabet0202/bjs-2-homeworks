94c0567a5822dd489f3b8abe235f6994c712b72b
Elizabet0202 <yelizaveta.baklanova.00@gmail.com>
2026-01-17 22:17:46 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length != arr2.length) {
+        return false;
+    }
+    return arr1.every((element, index) => element === (arr2[index]))
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length === 0) {
+        return 0;
+    }
+    if (gender !== "мужской" && gender !== "женский") {
+        return 0;
+    }
+    let filteredUsers = users.filter((user) => user.gender === gender)
+    return filteredUsers.reduce((acc, user) => acc + user.age, 0) / filteredUsers.length;
 }
